0ea8c7a4e6af0c5ff6136bf4b141bb91b5e8e774
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 12:57:57 -0600

    New Store (stores): :sparkles: user.js was created

---
+++ src/stores/user.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+const useuserStore = defineStore('userStore', {
+    state: () => ({
+        userData: 'bluuweb@test.com'
+    }),
+    actions: {
+
+    }
+})
