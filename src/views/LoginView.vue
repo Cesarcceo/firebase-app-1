dcca592797dc0104151fb1ef5789a61546ea1d70
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 15:23:44 -0600

    New feature (LoginView.vue): :sparkles: login implemented

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,33 @@
 <script setup>
+  import { ref } from 'vue'
+  import { useUserStore } from '../stores/user'
+  import { useRouter } from 'vue-router'
+  
+  const email = ref('')
+  const password = ref('')
+
+  const userStore = useUserStore()
+  const router = useRouter()
+
+  const handleSubmit = async() => {
+    if(!email.value || password.value.length < 8){
+      return alert('Email or Passwor incorrect')
+    }
+    await userStore.loginUser(email.value, password.value)
+    router.push('/')
+  }
 
 </script>
 
 <template>
   <div>
-    Login
+    <h1>Login</h1>
+    <form @submit.prevent="handleSubmit">
+      <input type="email" placeholder="enter email" v-model.trim="email">
+      <input type="password" placeholder="enter password" v-model.trim="password">
+      <button type="submit" >Login</button>
+    </form>
   </div>
 </template>
+
 
