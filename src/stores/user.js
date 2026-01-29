6524642cee87cd04cddf787f0f475076fc2c4e56
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 15:45:18 -0600

    New feature (user.js): :sparkles: loadingUser state implemented

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,31 +4,41 @@ import { auth } from "../firebaseConfig";
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
-        userData: null
+        userData: null,
+        loadingUser: false
     }),
     actions: {
         async registeruser(email, password){
+            this.loadingUser = true
             try {
                 const { user } = await createUserWithEmailAndPassword(auth, email, password) 
                 this.userData = {email: user.email, uid: user.uid}            
             } catch (e) {
                 console.log(e)
+            }finally{
+                this.loadingUser = false
             }
         },
         async loginUser(email, password){
+            this.loadingUser = true
             try {
                 const {user} = await signInWithEmailAndPassword(auth, email, password)
                 this.userData = { email: user.email, password: user.password }
             } catch (e) {
                 console.log(e)
+            } finally{ 
+                this.loadingUser = false
             }
         },
         async logoutUser(){
+            this.loadingUser = true
             try {
                 await signOut(auth)
                 this.userData = null
             } catch (e) {
                 console.log(e)
+            } finally {
+                this.loadingUser = true
             }
         }
     }
