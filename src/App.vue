10b3fe9a3c1bae947d6f624990c92af05ed0b0c8
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 12:47:39 -0600

    New feature (App.vue): :sparkles: routes implemented

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@
 
 <template>
   <div>
-    APP Base
+    <h1>APP Base</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <button>Logout</button>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
