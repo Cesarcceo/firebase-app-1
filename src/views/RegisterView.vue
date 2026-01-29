9e4374604ac0f14a5da994aea110c39df6a0ab50
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 14:59:21 -0600

    New feature (Registerview.vue): :sparkles: Athentification implemented

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,10 +1,29 @@
 <script setup>
+  import { ref } from 'vue'
+  import { useUserStore } from '../stores/user'
+  
+  const email = ref('')
+  const password = ref('')
+
+  const userStore = useUserStore()
+
+  const handleSubmit = () => {
+    if(!email.value || password.value.length < 8){
+      return alert('Email or Passwor incorrect')
+    }
+    userStore.registeruser(email.value, password.value)
+  }
 
 </script>
 
 <template>
   <div>
-    Register
+    <h1>Register</h1>
+    <form @submit.prevent="handleSubmit">
+      <input type="email" placeholder="enter email" v-model.trim="email">
+      <input type="password" placeholder="enter password" v-model.trim="password">
+      <button type="submit" >Create user</button>
+    </form>
   </div>
 </template>
 
