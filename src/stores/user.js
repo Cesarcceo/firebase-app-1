f2e3ff872cdbd6c39f4096ea25267f73186731d1
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 14:36:23 -0600

    New feature (user.js): :sparkles: implement authetification metod

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,10 +1,19 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { defineStore } from "pinia";
+import { auth } from "../firebaseConfig";
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
-        userData: 'bluuweb@test.com'
+        userData: null
     }),
     actions: {
-
+        async registeruser(email, password){
+            try {
+                const { user } = await createUserWithEmailAndPassword(auth, EmailAuthCredential, password) 
+                console.log(user)               
+            } catch (e) {
+                console.log(e)
+            }
+        }
     }
 })
