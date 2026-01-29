aaacd067b819ebcd8b6d0051115703e2d8bcd042
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-29 15:40:53 -0600

    New feature (App.vue): :sparkles: loguot implemented

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
+  import { useRouter } from 'vue-router'
+import { useUserStore } from './stores/user'
 
+  const userStore = useUserStore()
+  const router = useRouter()
+
+  const logout = async() => {
+    await userStore.logoutUser()
+    router.push('/login')
+  }
 </script>
 
 <template>
@@ -9,7 +18,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/register">Register</router-link> |
-      <button>Logout</button>
+      <button @click="logout">Logout</button>
     </nav>
     <router-view></router-view>
   </div>
