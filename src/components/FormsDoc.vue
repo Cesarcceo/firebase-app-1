4c88edb06616ffe7e6c915c8df4b322e83bd82e1
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 16:20:40 -0600

    New Component (components): :sparkles: FormsDoc was added

---
+++ src/components/FormsDoc.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { useDatabaseStore } from '../stores/database';
+    import { ref } from 'vue';
+
+    const databaseStore = useDatabaseStore()
+
+    const url = ref('')
+
+    const handleSubmit = ()=> {
+        databaseStore.addUrl(url.value)
+    }
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="text" placeholder="URL" v-model="url">
+        <button type="submit">Save</button>
+    </form>
+</template>
