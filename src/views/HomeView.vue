d3b9c93a2b4b077df7149808cdbd7a2786070de4
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 14:44:41 -0600

    New feature (HomeView.vue): :sparkles: now the documents are showing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,21 @@
 <script setup>
+  import { useDatabaseStore } from '../stores/database';
 
+  const databaseStore = useDatabaseStore()
+
+  databaseStore.getUrls()
 </script>
 
 <template>
   <div>
-    Home
+    <h1>Home</h1>
+    <ul>
+      <li v-for="item in databaseStore.documents" :key="item.id">
+        {{item.id}} - {{ item.name }}
+        <br>
+        {{ item.short }}
+      </li>
+    </ul>
   </div>
 </template>
 
