a392b7f384dcbcfc518bce48f357de0ff1680e10
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 13:37:18 -0600

    New feature (App.vue): :sparkles: routes desapier depends of the state of the loading session

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,15 @@ import { useUserStore } from './stores/user'
 <template>
   <div>
     <h1>APP Base</h1>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/register">Register</router-link> |
-      <button @click="logout">Logout</button>
+    <nav v-if="!userStore.loadingSession">
+      <router-link v-if="userStore.userData" to="/">Home</router-link> | 
+      <router-link v-if="!userStore.userData" to="/login">Login</router-link> |
+      <router-link v-if="!userStore.userData" to="/register">Register</router-link> |
+      <button v-if="userStore.userData" @click="logout">Logout</button>
     </nav>
+    <div v-else>
+      <h1>Loading...</h1>
+    </div>
     <router-view></router-view>
   </div>
 </template>
