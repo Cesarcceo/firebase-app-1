3acdfacbe6dc55a9616b93b5a96c5a96eadeda88
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 13:07:45 -0600

    New feature (router.js): :sparkles: proctection the route home with the Authentification

--- src/router.js
+++ src/router.js
@@ -1,17 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "./stores/user";
 import HomeView from "./views/HomeView.vue";
 import LoginView from "./views/LoginView.vue";
 import RegisterView from "./views/RegisterView.vue";
 
+const requireAuth = async (to, from, next) => {
+    const userStore = useUserStore()
+    const user = await userStore.currentUser()
+    if(user){
+        next()
+    } else {
+        next('/login')
+    }
+    
+}
+
 const routes = [
-    {path: '/', component: HomeView},
+    {path: '/', component: HomeView, beforeEnter: requireAuth},
     {path: '/login', component: LoginView},
     {path: '/register', component: RegisterView},
 ]
 
 const router = createRouter({
-    routes,
     history: createWebHistory(),
+    routes,
 })
 
 export default router;
