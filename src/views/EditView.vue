78e5f1abee619af72e437682732981a7329045f7
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 17:03:14 -0600

    New View (views): :sparkles: EditView.vue was created

---
+++ src/views/EditView.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { useRoute } from 'vue-router';
+    const route = useRoute()
+
+    console.log(route.params.id)
+
+    const handleSubmit = () => {
+        console.log('edito la url')
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Edit View</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="text" placeholder="Enter the url">
+            <button type="submit">Edit</button>
+        </form>
+    </div>
+</template>
