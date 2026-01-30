f9ce43f40b1aa247304ca7e9b9f3b32a11b8c921
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 16:43:04 -0600

    New feature (HomoView.vue): :sparkles: remove button implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,9 +13,12 @@
     <p v-if="databaseStore.loadingDoc">loading...</p>
     <ul v-else>
       <li v-for="item in databaseStore.documents" :key="item.id">
-        {{item.id}} - {{ item.name }}
+        {{item.id}}
+        <br>
+        {{ item.name }}
         <br>
         {{ item.short }}
+        <button class="my-3" @click="databaseStore.removUrl(item.id)">Remove</button>
       </li>
     </ul>
     <FormsDoc/>
