8670fc4d77b27a183658616b6e5fd9eb755b34f5
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-01-30 17:34:34 -0600

    New feature (EditView.vue): :sparkles: reedUrl implement

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,19 +1,27 @@
 <script setup>
     import { useRoute } from 'vue-router';
-    const route = useRoute()
+    import { useDatabaseStore } from '../stores/database';
+    import { onMounted, ref } from 'vue';
 
-    console.log(route.params.id)
+    const route = useRoute()
+    const databaseStore = useDatabaseStore()
 
     const handleSubmit = () => {
         console.log('edito la url')
     }
+
+    const url = ref('')
+
+    onMounted(async() => {
+        url.value = await databaseStore.reedUrl(route.params.id)
+    })
 </script>
 
 <template>
     <div>
         <h1>Edit View</h1>
         <form @submit.prevent="handleSubmit">
-            <input type="text" placeholder="Enter the url">
+            <input type="text" placeholder="Enter the url" v-model="url">
             <button type="submit">Edit</button>
         </form>
     </div>
