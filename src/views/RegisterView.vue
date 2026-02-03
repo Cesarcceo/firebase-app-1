283548a4734759240bfefc3ce37969769c5eacec
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-03 15:14:30 -0600

    New feature (RegisterView.vue): :sparkles: change the style for ant desing vue style and add a repeat password input

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,33 +1,88 @@
 <script setup>
-  import { ref } from 'vue'
+  import { reactive } from 'vue'
   import { useUserStore } from '../stores/user'
   import { useRouter } from 'vue-router'
-  
-  const email = ref('')
-  const password = ref('')
 
   const userStore = useUserStore()
   const router = useRouter()
 
-  const handleSubmit = async() => {
-    if(!email.value || password.value.length < 8){
-      return alert('Email or Passwor incorrect')
-    }
-    await userStore.registeruser(email.value, password.value)
-    router.push('/')
-  }
+  const formState = reactive({
+    email: '',
+    password: '',
+    repassword: '',
+  })
 
+  const onFinish = async values => {
+    console.log('Success:', values);
+    await userStore.registeruser(formState.email, formState.password)
+    router.push('/')
+  };
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const validatePass = async (_rule, value) => {
+    if(value === ''){
+      return Promise.reject('Repit the password')
+    }
+    if (value !== formState.password) {
+      return Promise.reject('The passwords no match')
+    }
+    return Promise.resolve()
+  }
 </script>
 
 <template>
-  <div>
-    <h1>Register</h1>
-    <form @submit.prevent="handleSubmit">
-      <input type="email" placeholder="enter email" v-model.trim="email">
-      <input type="password" placeholder="enter password" v-model.trim="password">
-      <button type="submit" :disabled="userStore.loadingUser">Create user</button>
-    </form>
-  </div>
+  <a-row>
+    <a-col span="12" offset="6">
+      <h1>Register</h1>
+      <a-form
+        name="basicRegister"
+        autocomplete="off"
+        layout="vertical"
+        :model="formState"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+      >
+      <a-form-item
+          name="email"
+          label="Email"
+          :rules="[{
+            required: true, 
+            whitespace:true, 
+            type: 'email',
+            message: 'Enter the email'
+            }]"
+        >
+          <a-input v-model:value="formState.email"></a-input>
+        </a-form-item>
+        <a-form-item
+          name="password"
+          label="Password"
+          :rules="[{
+            required: true, 
+            min:8, 
+            whitespace: true, 
+            message: 'Enter the password'
+            }]"
+        >
+          <a-input-password v-model:value="formState.password"></a-input-password>
+        </a-form-item>
+        <a-form-item
+          name="repassword"
+          label="Repeat Password"
+          :rules="[{
+            required: true, 
+            validator: validatePass,
+            }]"
+        >
+          <a-input-password v-model:value="formState.repassword"></a-input-password>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" :disabled="userStore.loadingUser">Register</a-button>
+        </a-form-item>
+
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
-
-
