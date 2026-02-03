6a47adff41a534d7a5c7cb252d0aa12542141ebf
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-03 16:37:13 -0600

    New feature (FormsView.vue): :sparkles: change the style for ant desing vue style and add a url validation

--- src/components/FormsDoc.vue
+++ src/components/FormsDoc.vue
@@ -1,19 +1,40 @@
 <script setup>
     import { useDatabaseStore } from '../stores/database';
-    import { ref } from 'vue';
+    import { reactive } from 'vue';
 
     const databaseStore = useDatabaseStore()
 
-    const url = ref('')
+    const formState = reactive({
+        url: ''
+    })
 
-    const handleSubmit = ()=> {
-        databaseStore.addUrl(url.value)
+    const onFinish = ()=> {
+        databaseStore.addUrl(formState.url)
     }
 </script>
 
 <template>
-    <form @submit.prevent="handleSubmit">
-        <input type="text" placeholder="URL" v-model="url">
-        <button type="submit">Save</button>
-    </form>
+    <a-form
+        name="saveUrl"
+        autocomplete="off"
+        layout="vertical"
+        :model="formState"
+        @finish="onFinish"
+    >
+        <a-form-item
+            name="url"
+            label="New Url"
+            :rules="[{
+                required:true,
+                whitespace:true,
+                pattern: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+                message: 'Enter a valid url'
+            }]"
+        >
+            <a-input v-model:value="formState.url"></a-input>
+        </a-form-item>
+        <a-form-item>
+            <a-button type="primary" html-type="submit">Save</a-button>
+        </a-form-item>
+    </a-form>
 </template>
