b6dbcf958d4d7ca08c30838c98836d43bb54de2d
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-03 17:24:58 -0600

    Refactor Code (HomeView.vue): :sparkles: implement ListDoc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,13 @@
 <script setup>
-  import { useDatabaseStore } from '../stores/database';
   import FormsDoc from '../components/FormsDoc.vue'
-  import { useRouter } from 'vue-router';
-
-  const databaseStore = useDatabaseStore()
-  const router = useRouter()  
-
-  databaseStore.getUrls()
+  import ListDoc from '../components/ListDoc.vue';
 </script>
 
 <template>
   <div>
     <h1>Home</h1>
     <FormsDoc/>
-    <p v-if="databaseStore.loadingDoc">loading...</p>
-    <ul v-else>
-      <li v-for="item in databaseStore.documents" :key="item.id">
-        {{item.id}}
-        <br>
-        {{ item.name }}
-        <br>
-        {{ item.short }}
-        <br>
-        <button @click="databaseStore.removUrl(item.id)">Remove</button>
-        <button @click="router.push(`/edit/${item.id}`)">Edit</button>
-      </li>
-    </ul>
+    <ListDoc/>
   </div>
 </template>
 
